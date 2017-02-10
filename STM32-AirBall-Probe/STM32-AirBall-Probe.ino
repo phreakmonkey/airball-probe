544319ece3e1465c35fcc5d68c5b6b7e110b686e
K.C. Budd <phreakmonkey@gmail.com>
2017-02-09 17:04:13 -0800

    Addig calibration capability

--- STM32-AirBall-Probe/STM32-AirBall-Probe.ino
+++ STM32-AirBall-Probe/STM32-AirBall-Probe.ino
@@ -2,13 +2,13 @@
     Created by Kenneth C. Budd - phreakmonkey@gmail.com
 **/
 
-
 #include <Wire.h>
 #include <Adafruit_BMP085.h>
 
 /** Build-time Config Values 
     LED = pin for LED status indicator.
     SENSOR1, SENSOR2, SENSOR3 = Analog input pin assignments
+    BAUDRATE = Serial baud rate
     TIMERA = how often (in ms) to run timerA routines
     TIMERB = how often (in ms) to run timerB routines
 **/
@@ -17,6 +17,8 @@
 #define SENSOR1 PB1
 #define SENSOR2 PB0
 #define SENSOR3 PA7
+
+#define BAUDRATE 115200
 
 #define TIMERA 50
 #define TIMERB 200
@@ -28,6 +30,9 @@ Adafruit_BMP085 bmp;
 uint16_t sensor1avg;
 uint16_t sensor2avg;
 uint16_t sensor3avg;
+int16_t sensor1cal = 0;
+int16_t sensor2cal = 0;
+int16_t sensor3cal = 0;
 
 uint32_t timerA = 0;
 uint32_t timerB = 0;
@@ -44,13 +49,15 @@ void setup() {
     sensor2avg = AnalogSensor(SENSOR2);
     sensor3avg = AnalogSensor(SENSOR3);
 
-    Serial1.begin(115200);  // STM32F103C8 pins PA2/A2(tx) PA3/A3(rx)
+    Serial1.begin(BAUDRATE);  // STM32F103C8 pins PA2/A2(tx) PA3/A3(rx)
     if (!bmp.begin()) {
         Serial1.println("Could not find a valid BMP085 sensor, check wiring!");
     }
 }
 
 void loop() {
+    int32_t altitude;
+    float temperature;
 
     if (millis() - timerA >= TIMERA) {
         timerA = millis();
@@ -62,7 +69,14 @@ void loop() {
     if (millis() - timerB >= TIMERB) {
         timerB = millis();
         digitalWrite(LED,!digitalRead(LED)); // Toggle status LED
-        OutputSentence(sensor1avg, sensor2avg, sensor3avg);
+        altitude = bmp.readAltitude();
+        temperature = bmp.readTemperature();
+        OutputSentence(
+            sensor1avg + sensor1cal, 
+            sensor2avg + sensor2cal, 
+            sensor3avg + sensor3cal, 
+            altitude, 
+            temperature);
     }
 }
 
@@ -75,7 +89,7 @@ uint16_t AnalogSensor(int iopin)
   return reading;
 }
 
-void OutputSentence(uint16_t s1, uint16_t s2, uint16_t s3)
+void OutputSentence(uint16_t s1, uint16_t s2, uint16_t s3, int32_t alt, float temp)
 {
   Serial1.print("@");
   Serial1.print(s1);
@@ -84,9 +98,9 @@ void OutputSentence(uint16_t s1, uint16_t s2, uint16_t s3)
   Serial1.print(",");
   Serial1.print(s3);
   Serial1.print(",");
-  Serial1.print(bmp.readAltitude());  // Pressure Altitude (29.92in SLP)
+  Serial1.print(alt);  // Pressure Altitude (29.92in SLP)
   Serial1.print(",");
-  Serial1.print(bmp.readTemperature());  // Temp of BMP180 sensor
+  Serial1.print(temp);  // Temp of BMP180 sensor
   Serial1.println();
 }
 
